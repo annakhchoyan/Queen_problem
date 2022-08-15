41180f1ab557564a92abb18a1b7e64a857288961
annakhchoyan <105013630+annakhchoyan@users.noreply.github.com>
2022-08-15 14:41:18 +0400

    Update Queen_problem.cpp

--- Queen_problem.cpp
+++ Queen_problem.cpp
@@ -4,79 +4,87 @@ const int n = 8;
 
 void printSolution(int board[n][n])
 {
-	for(int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		for(int j = 0; j < n; ++j)
+		for (int j = 0; j < n; ++j)
 		{
 			std::cout << " " << board[i][j] << " ";
 		}
+		
 		std::cout << std::endl;
 	}
 }
 
 bool isSafe(int board[n][n], int row, int col)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for(i = 0; i < col; ++i)
+	for (i = 0; i < col; ++i)
 	{
-		if(board[row][i])
+		if (board[row][i])
 		{
 			return false;
 		}
 	}
-	for(i = row, j = col; i >= 0 && j >= 0; --i, --j)
+	for (i = row, j = col; i >= 0 && j >= 0; --i, --j)
 	{
-		if(board[i][j])
+		if (board[i][j])
 		{
 			return false;
 		}
 	}
-	for(i = row, j = col; j >= 0 && i < n; ++i, --j)
+	for (i = row, j = col; j >= 0 && i < n; ++i, --j)
 	{
-		if(board[i][j])
+		if (board[i][j])
 		{
 			return false;
 		}
 	}
+	
 	return true;
 }
 
 bool solveNQUtil(int board[n][n], int col)
 {
-	if(col >= n)
+	if (col >= n)
 	{
 		return true;
 	}
-	for(int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		if(isSafe(board, i, col))
+		if (isSafe(board, i, col))
 		{
 			board[i][col] = 1;
-			if(solveNQUtil(board, col + 1))
+			
+			if (solveNQUtil(board, col + 1))
 			{
 				return true;
 			}
+			
 			board[i][col] = 0;
 		}
 	}
+	
 	return false;
 }
 
 bool solveNQ()
 {
 	int board[n][n] = { {0, 0, 0, 0},
-						{0, 0, 0, 0},
-						{0, 0, 0, 0},
-						{0, 0, 0, 0} };
+	       		    {0, 0, 0, 0},
+			    {0, 0, 0, 0},
+	           	    {0, 0, 0, 0} };
 	
-	if(solveNQUtil(board, 0) == false)
+	if (solveNQUtil(board, 0) == false)
 	{
 		std::cout << "Solution does not exist";
+	
 		return false;
 	}
 
 	printSolution(board);
+	
 	return true;
 }
 
